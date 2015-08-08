6171b367146a12e41e9a8ba0ec38aacb93ba6098
snascs <samuel_ns@msn.com>
2015-08-08 10:23:21 -0300

    commit final

--- faq/urls.py
+++ faq/urls.py
@@ -8,6 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', views.ListarFaq.as_view(), name='listar_faq'),
     url(r'^novo$', views.CriarFaq.as_view(), name='criar_faq'),
-    url(r'^editar/(?P<pk>\d+)$', views.AlterarFaq.as_view(), name='editar_faq'),
+    url(r'^editar/(?P<pk>\d+)$', views.AlterarFaq.as_view(), name='alterar_faq'),
     url(r'^deletar/(?P<pk>\d+)$', views.DeletarFaq.as_view(), name='deletar_faq'),
 )
