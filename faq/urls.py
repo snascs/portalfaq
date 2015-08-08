88a66aa17121153ac68bd0bfa8a7ff8c674fe2da
snascs <samuel_ns@msn.com>
2015-08-08 09:12:08 -0300

    models, views e urls aplicacao

---
+++ faq/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from cadastro import views
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', views.ListarFaq.as_view(), name='listar_faq'),
+    url(r'^novo$', views.CriarFaq.as_view(), name='criar_faq'),
+    url(r'^editar/(?P<pk>\d+)$', views.AlterarFaq.as_view(), name='editar_faq'),
+    url(r'^deletar/(?P<pk>\d+)$', views.DeletarFaq.as_view(), name='deletar_faq'),
+)
--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+from django.view.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.models import Faq
 
-# Create your views here.
+class ListarFaq(ListView):
+	model = Faq
+
+class CriarFaq(CreateView):
+	model = Faq
+	fields = ['pergunta', 'resposta', 'data']
+	success_url = reverse_lazy('faq:listar_faq')
+
+class AlterarFaq(UpdateView):
+	model = Faq
+	fields = ['pergunta', 'resposta', 'data']
+	success_url = reverse_lazy('faq:listar_faq')
+
+class DeletarFaq(DeleteView):
+	model = Faq
+	fields = ['pergunta', 'resposta', 'data']
+	success_url = reverse_lazy('faq:listar_faq')
