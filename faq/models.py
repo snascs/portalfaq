4c56fda27ef8d82719c0cd0135aaf71ff97114b5
snascs <samuel_ns@msn.com>
2015-08-08 08:59:57 -0300

    alteracao views aplicacao

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Faq(models.Model):
+	pergunta = models.CharField('Pergunta', max_length=200)
+    resposta = models.CharField('Resposta', max_length=200)
+    data = DateField('Data') 
+
+    def __unicode__(self):
+    	return self.nome
+
+    def get_absolute_url(self):
+    	return reverse ('faq:editar_faq', kwargs('pk':self.pk))
